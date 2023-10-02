cda6f43074a92d336e36fd313fa0b71bf0aaaa42
Home Dev <nshkrdotcom@nshkr.com>
2023-10-02 08:13:25 -1000

    refactor

--- installApps.sh
+++ installApps.sh
@@ -1,24 +1,6 @@
 #!/bin/zsh
+source ./_functions.sh
 
-function install_casks {
-  # Install apps to /Applications, Default is: /Users/$user/Applications
-  for appName in "$@"
-  do
-    echo 'Installing cask: '"$appName"
-    brew install --cask --appdir="/Applications" "$appName"
-  done
-}
-
-function install_nocasks {
-  echo "Installing apps with Brew Cask..." 
-  for appName in "$@"
-  do
-    echo 'Installing app '"$appName"' with Brew...'
-    brew install --appdir="/Applications" "$appName"
-  done
-}
-
-USER=`whoami`
 echo 'Installing apps with user: '${USER}
 
 echo "Installing XCode Command Line Tools"
--- installAppsDataEngineering.sh
+++ installAppsDataEngineering.sh
@@ -1,24 +1,5 @@
 #!/bin/zsh
-
-function install_casks {
-  # Install apps to /Applications, Default is: /Users/$user/Applications
-  for appName in "$@"
-  do
-    echo 'Installing cask: '"$appName"
-    brew install --cask --appdir="/Applications" "$appName"
-  done
-}
-
-function install_nocasks {
-  echo "Installing apps with Brew Cask..." 
-  for appName in "$@"
-  do
-    echo 'Installing app '"$appName"' with Brew...'
-    brew install --appdir="/Applications" "$appName"
-  done
-}
-
-USER=`whoami`
+source ./_functions.sh
 
 echo 'Installing rust...'
 brew install rustup
