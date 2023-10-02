26f2ef4b60130309901c842274ead167e439eab8
Home Dev <nshkrdotcom@nshkr.com>
2023-10-02 08:27:51 -1000

    refactor install apps

--- installApps.sh
+++ installApps.sh
@@ -73,71 +73,7 @@ brew cleanup
 
 # https://proxyman.io/posts/2019-10-26-Alternatives-for-charles-proxy-and-wireshark
 
-apps=(
-#  alfred # brew --cask alfred link
-#  bettertouchtool
-#  cloud
-#  colloquy  
-#  cornerstone
-#  diffmerge
-#  dropbox
-#  filezilla
-#  harvest
-#  hipchat
-#  licecap
-#  mou
-#  phpstorm
-#  private-internet-access
-#  razer-synapse
-#  sourcetree
-#  steam
-#  spotify
-#  sublime-text2
-#  textexpander
-#  virtualbox
-#  mailbox
-#  vlc 
-#  skype
-#  transmission
-#  zoomus
-#  onepassword
-#  chromecast
-#  qlmarkdown  
-#  qlstephen
-#  suspicious-package 
-#  adguard
-#  messenger
-#  snapapi
-  bartender
-  #cleanmymac
-  firefox
-  microsoft-edge
-  google-chrome
-  vagrant
-  iterm2
-  sequel-pro
-  anaconda
-  android-studio
-  dbeaver-community
-  element
-  firefox
-  gimp
-  inkscape
-  krita
-  whatsapp
-  microsoft-excel
-  microsoft-powerpoint
-  microsoft-word
-  scribus
-  sloth
-  visual-studio-code
-  #virtualbox  
-  wireshark
-  #mactex
-)
-install_casks $apps
-
-brew cleanup
+./installAppsDesktop.sh
 
 ./installAppsPython.sh
 
